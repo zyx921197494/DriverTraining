9f8cdbe67af231021a865c3a8457489ece648763
zyx <921197494@qq.com>
2021-08-02 16:10:51 +0800

    2021年8月2日16:10:42

--- src/main/webapp/admindatas/nav.js
+++ src/main/webapp/admindatas/nav.js
@@ -4,11 +4,12 @@ var navs = [{
 	"spread": true,
 	"children": [{
 		"title": "学员预约记录",
-		"icon": "&#xe641;"
+		"icon": "&#xe641;",
+		"href": "http://localhost:8080/adminStuRecord.html"
 	}, {
 		"title": "教练上课记录",
 		"icon": "&#xe63c;",
-		"href": "form.html"
+		"href": "http://localhost:8080/index.html"
 	}]
 }, {
 	"title": "人员管理",
